8b1f5b8247512e3f7869ce9ca85dcef7d815c725
JoseWillians <87570173+JoseWillians@users.noreply.github.com>
2022-09-21 01:09:39 -0300

    Update app.js

--- node -str/SRC/app.js
+++ node -str/SRC/app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 
 const app = express()
 const router = express.Router()
 
 //Conecta ao banco de dados
-mongoose.connect('mongodb+srv://user-admin:1234@nodestr.7hfpfyb.mongodb.net/test')
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route')
